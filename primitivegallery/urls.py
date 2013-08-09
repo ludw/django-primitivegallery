7e6e68c3b9a4260285598a4c60c953eede57e0b7
Ludvig Wadenstein <ludvig.wadenstein@codemill.se>
2013-08-09 23:14:02 +0200

    Removed broken upload code

--- primitivegallery/urls.py
+++ primitivegallery/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns
 
 urlpatterns = patterns(
     'primitivegallery.views',
-    (r'^upload$', 'upload'),
     (r'^process$', 'process_image'),
     (r'^$', 'list'),
     (r'^(?P<size>thumbnails|small|medium)/(?P<subfolder>.*)$', 'list'),
--- primitivegallery/views.py
+++ primitivegallery/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render_to_response
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
 from primitivegallery.models import Image, Directory
-from django.http import HttpResponse, HttpResponseRedirect
-from django.core.urlresolvers import reverse
-from django.template import RequestContext
+from django.http import HttpResponse
 from datetime import datetime, timedelta
-from primitivegallery.forms import UploadFileForm
 
 
 def list(request, size='thumbnails', subfolder=''):
@@ -53,26 +50,3 @@ def process_image(request):
     return render_to_response('primitivegallery/process.html', {
         'count': count
     })
-
-
-def upload(request):
-    if request.method == 'POST':
-        form = UploadFileForm(request.POST, request.FILES)
-        if form.is_valid():
-            process_files(request.FILES.getlist('files'), request.POST.get('directory'))
-            return HttpResponseRedirect(reverse('imagegrid.views.upload'))
-    else:
-        form = UploadFileForm()
-
-    return render_to_response('primitivegallery/upload.html', {
-        'form': form
-    }, context_instance=RequestContext(request))
-
-
-def process_files(files, path):
-    print path
-    for f in files:
-        print f
-        #with open('some/file/name.txt', 'wb+') as destination:
-        #    for chunk in f.chunks():
-        #        destination.write(chunk)
